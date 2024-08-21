62b52d59c51fb297defab1c71fea47e1b9764d0d
devendrabogati <dbofficial0047@gmail.com>
2024-08-21 18:42:52 +0530

    completed data ingestion

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -11,7 +11,7 @@ from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.components.model_evaluation import ModelEvaluation
 from networksecurity.components.model_pusher import ModelPusher
 
-from  networksecurity.entity.config_entity import(
+from networksecurity.entity.config_entity import(
     TrainingPipelineConfig,
     DataIngestionConfig,
     DataValidationConfig,
@@ -19,28 +19,33 @@ from  networksecurity.entity.config_entity import(
     ModelTrainerConfig,
     ModelEvaluationConfig,
     ModelPusherConfig
+   
 )
 
-from networksecurity.entity.artifact_entity import(
+from networksecurity.entity.artifact_entity import (
     DataIngestionArtifact,
     DataValidationArtifact,
     DataTransformationArtifact,
     ModelTrainerArtifact,
     ModelEvaluationArtifact,
     ModelPusherArtifact
-    
 )
+
 
 class TrainingPipeline:
     def __init__(self):
-        try:
-            pass
-        except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
+         self.training_pipeline_config = TrainingPipelineConfig()
+    
     def start_data_ingestion(self):
         try:
-            pass
+            self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Starting data ingestion")
+            data_ingestion=DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
+            
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -55,18 +60,18 @@ class TrainingPipeline:
             pass
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-        
-    def start_model_trainer(self,config):
+    
+    def start_model_trainer(self):
         try:
             pass
         except Exception as e:
-            raise NetworkSecurityException(e,sys)   
+            raise NetworkSecurityException(e,sys)
         
-    def start_model_evaluation(self):     
+    def start_model_evaluation(self):
         try:
             pass
         except Exception as e:
-            raise NetworkSecurityException(e,sys)   
+            raise NetworkSecurityException(e,sys)
         
     def start_model_pusher(self):
         try:
@@ -76,7 +81,9 @@ class TrainingPipeline:
         
     def run_pipeline(self):
         try:
-            pass
+            data_ingestion_artifact=self.start_data_ingestion()
+            print(data_ingestion_artifact)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-            
+        
+    
